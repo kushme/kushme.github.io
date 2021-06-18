da0173324a1e6cb2ed61e0a424ad8289700fb19c
Build changes <>
2021-06-18 22:39:53 +0530

    Hot

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -253,9 +253,9 @@ const RESOURCES = {
 "firebase-messaging-sw.js": "e3cb633ced387d5a461ec4350cb85275",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "8a13c6dc4880e46b87316d66896861c4",
-"/": "8a13c6dc4880e46b87316d66896861c4",
-"main.dart.js": "45700bcc864d3b77d44403a4e2715dab",
+"index.html": "53959218d03d3c79ee22932e7e312cdf",
+"/": "53959218d03d3c79ee22932e7e312cdf",
+"main.dart.js": "24acc5192d849b7a1318d9fd79df3fc6",
 "manifest.json": "82c1b3d1720db1d7e16aa67960400e42",
 "version.json": "5e18e19887d62341063565b772de8746"
 };
